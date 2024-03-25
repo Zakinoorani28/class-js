093266c564ab989048519e6c64aa62c9b171e21c
Zaki Noorani <zakinoorani2006@gmail.com>
2024-03-25 15:20:57 +0500

    final done

--- assignment/script.js
+++ assignment/script.js
@@ -1,41 +1,25 @@
 const userInput = document.getElementById('input');
+
 function number_cardinality() {
     if (!isNaN(userInput.value)) {
         alert(checkCardinality(parseInt(userInput.value)));
     } else {
-        alert("Invalid input. Please enter a valid integer!!");
+        alert("Invalid input. Please enter a valid integer!!😠😠");
     }
 }
 
 function checkCardinality(number) {
     if (number % 10 === 0) {
+        userInput.value = '';
         return "zero";
-        userInput.value = ''
     } else if (number % 10 === 5) {
+        userInput.value = '';
         return "five";
     } else if (number % 2 === 0) {
+        userInput.value = '';
         return "even";
-    } else {
+    } else if (number % 2 !== 0) {
+        userInput.value = '';
         return "odd";
     }
 }
-
-// function number_cardinality(number) {
-//   if (number % 10 === 0) {
-//     return "zero";
-//   } else if (number % 10 === 5) {
-//     return "five";
-//   } else if (number % 2 === 0) {
-//     return "even";
-//   } else {
-//     return "odd";
-//   }
-// }
-
-// const userInput = document.querySelector('input');
-
-// if (!isNaN(userInput.value)) {
-//   alert(number_cardinality(userInput.value));
-// } else {
-//   alert("Invalid input. Please enter a valid integer!! 😠😠");
-// }
