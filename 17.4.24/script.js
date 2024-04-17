966826f38ba0f878ad50e9de3d89ba7476c74e92
Zaki Noorani <zakinoorani2006@gmail.com>
2024-04-17 15:27:35 +0500

    new class

---
+++ 17.4.24/script.js
@@ -0,0 +1,36 @@
+// var getDay = getDay();
+// var getMonth = getMonth();
+// var getFullYear = getFullYear();
+// var getDate = getDate();
+// var getHours = getHours();
+// var getMinutes = getMinutes();
+// var getSeconds = getSeconds();
+// var getMilliseconds = getMilliseconds();
+// var getTime = getTime();
+
+
+
+
+// let months = ["January","Fabuary","March","April","May","June","July","August","September","October","November","December"]; 
+// for(let i = 0; i < months.length; i++){
+//     console.log(months[i]);
+// }
+
+
+// console.log(months[(0)]);
+// console.log(months[(1)]);
+// console.log(months[(2)]);
+// console.log(months[(3)]);
+// console.log(months[(4)]);
+// console.log(months[(5)]);
+// console.log(months[(6)]);
+// console.log(months[(7)]);
+// console.log(months[(8)]);
+// console.log(months[(9)]);
+// console.log(months[(10)]);
+// console.log(months[(11)]);
+
+
+// var month = new months();
+// var theMonth = month.getMonths();
+// var nameofmonth = month.getMonthName();
