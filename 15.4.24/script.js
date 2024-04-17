d5e84fc9cba7ffd3d2110861dc77be02d888022e
Zaki Noorani <zakinoorani2006@gmail.com>
2024-04-17 14:09:40 +0500

    new

---
+++ 15.4.24/script.js
@@ -0,0 +1,19 @@
+// var newResult = +prompt("Enter a number");
+// console.log("🚀 ~ newResult:", newResult)
+
+// var afterRound = Math.round(newResult);
+// console.log("🚀 ~ afterRound:", afterRound)
+
+// var afterCeil = Math.ceil(newResult);
+// console.log("🚀 ~ afterCeil:", afterCeil)
+
+// var afterfloor = Math.floor(newResult);
+// console.log("🚀 ~ afterfloor:", afterfloor)
+
+
+// console.log(parseInt("20")+ 18)
+
+// console.log(parseFloat("20.555"))
+// console.log(parseFloat("20.555").toFixed(2))
+// console.log(parseFloat("20.555").toFixed(1))
+
