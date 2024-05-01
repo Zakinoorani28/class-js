c3b63ccb17ae9bb4181f279ce37b224f7bcf8f84
Zaki Noorani <zakinoorani2006@gmail.com>
2024-05-01 14:08:50 +0500

    NEW FOLDER ADDED

---
+++ 26.4.24/script.js
@@ -0,0 +1,15 @@
+// ASSIGNMENT 1 
+
+// function heart() {
+//     alert ("I LOVE YOU!");
+// }
+
+// ASSIGNMENT 2 
+
+// function card(tag) {
+//     tag.src = "https://playingcardcollector.files.wordpress.com/2014/10/venexiana-dark-playing-cards-reveal-edition-back-k.jpg?w=584"
+// }
+
+// function change(tag) {
+//     tag.src = "https://i.pinimg.com/736x/fe/8c/50/fe8c50b85306b9e400e7bdfa9439fb64.jpg"
+// }
