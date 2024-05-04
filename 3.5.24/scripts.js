56482a457240f554c46089929f3729aa88d17fc9
Zaki Noorani <zakinoorani2006@gmail.com>
2024-05-04 13:15:56 +0500

    new folder added

---
+++ 3.5.24/scripts.js
@@ -0,0 +1,18 @@
+// Assignment 1
+
+var body = document.getElementById("background");
+function red() {
+    body.style.backgroundColor = "red";
+}
+
+function green() {
+    body.style.backgroundColor = "green";
+}
+
+function blue() {
+    body.style.backgroundColor = "blue";
+}
+
+function reset() {
+    body.innerHTML = "";
+}
